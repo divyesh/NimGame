856ba2c09f8ac2fca83288dd1498b475c229bce0
divyesh kokani <divyeshkonkani@gmail.com>
2015-03-25 20:31:31 -0400

    Initial Commit

---
+++ NimGame/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  NimGame
+//
+//  Created by divyesh konkani on 2015-03-25.
+//  Copyright (c) 2015 divyesh konkani. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
